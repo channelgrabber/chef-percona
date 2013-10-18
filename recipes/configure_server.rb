9cd5715c53530395c4d2fef9ac6d2cc593a67711
Adam <adamb@channelgrabber.com>
2013-10-18 09:53:09 +0100

    restart only if config doesn't exist

--- recipes/configure_server.rb
+++ recipes/configure_server.rb
@@ -63,7 +63,7 @@ template percona["main_config_file"] do
   owner "root"
   group "root"
   mode 0744
-  notifies :restart, "service[mysql]"
+  notifies :restart, "service[mysql]", :delayed if !File.exists?(percona["main_config_file"])
 end
 
 # now let's set the root password only if this is the initial install
