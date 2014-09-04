c1a6c5de037b2a66653541538dc9ec2974f8a459
Richard Heelin <richardh@channelgrabber.com>
2014-09-04 10:53:37 +0100

    CGIV-3138: Added a new recipe that will install percona server from a deb package rather than through apt

---
+++ attributes/server_deb.rb
@@ -0,0 +1,2 @@
+default['percona']['server_deb']['version'] = "5.6.19-67.0-618"
+default['percona']['server_deb']['tmp'] = "/tmp/percona"
---
+++ recipes/server_deb.rb
@@ -0,0 +1,38 @@
+# Install exact version of percona by downloading deb packages
+
+serverVersion = node['percona']['server']['version']
+versionBuild = node['percona']['server_deb']['version']
+version = versionBuild.rpartition('-').first
+tmp = node['percona']['server_deb']['tmp']
+
+directory tmp
+%w{percona-server-common percona-server-client percona-server-server}.each do |package|
+    deb = File.join(tmp, "#{package}-#{serverVersion}_#{version}.deb")
+    source = "http://www.percona.com/redir/downloads" +
+        "/Percona-Server-#{serverVersion}" +
+        "/Percona-Server-#{version}" +
+        "/binary" +
+        "/#{node['platform_family']}" +
+        "/#{node['platform']}" +
+        "/x86_64" +
+        "/percona-server-server-#{serverVersion}_#{versionBuild}.#{node['platform']}_amd64.deb"
+
+    remote_file deb do
+        source source
+        action :create_if_missing
+    end
+
+    apt_package "#{package}-#{serverVersion}" do
+        source deb
+        version version
+        action :install
+    end
+end
+
+
+include_recipe "percona::configure_server"
+
+# access grants
+include_recipe "percona::access_grants"
+
+include_recipe "percona::replication"
