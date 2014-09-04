21aa7e489bdb5e6276daaeac368a6d7abf659126
Richard Heelin <richardh@channelgrabber.com>
2014-09-04 15:11:32 +0100

    CGIV-3138: Can't install nothing via resource so manually executing instead

--- recipes/server_deb.rb
+++ recipes/server_deb.rb
@@ -41,10 +41,7 @@ dpkg_package "percona-server-server-#{serverVersion}" do
     action :install
 end
 
-apt_package "percona-server-server-#{serverVersion}" do
-    package_name ""
-    action :install
-end
+execute "apt-get install -f"
 
 include_recipe "percona::configure_server"
 
