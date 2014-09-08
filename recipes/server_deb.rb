6a06cf74b96ada057171db9d20a259756c0facbc
Richard Heelin <richardh@channelgrabber.com>
2014-09-08 08:47:25 +0100

    CGIV-3145: Only install percona if version does not match requested version

--- recipes/server_deb.rb
+++ recipes/server_deb.rb
@@ -5,6 +5,7 @@ versionBuild = node['percona']['server_deb']['version']
 version = versionBuild.rpartition('-').first
 tmp = node['percona']['server_deb']['tmp']
 deb_path = File.join(tmp, "#{serverVersion}_#{versionBuild}.#{node['lsb']['codename']}_amd64")
+version_path = File.join(tmp, "version")
 
 directory deb_path do
     recursive true
@@ -34,10 +35,16 @@ end
     package dependency
 end
 
+execute "percona --version" do
+    command "dpkg -l | grep '^ii' | grep percona-server-server | awk '{print $3}' 1>#{version_path} 2>/dev/null"
+    action :run
+end
+
 dpkg_package "percona-server-server-#{serverVersion}" do
     source deb_path
     version "#{versionBuild}.#{node['lsb']['codename']}"
     options "--recursive --force-depends --force-configure-any"
+    not_if "dpkg --compare-versions #{versionBuild}.#{node['lsb']['codename']} '=' $(cat #{version_path})"
     action :install
 end
 
